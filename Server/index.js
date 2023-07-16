e0275202174d880aa9f1150fde3e2b2640681c80
Victor Kipkorir <victorquaint@gmail.com>
2023-07-17 00:36:12 +0300

    Responsive

--- Server/index.js
+++ Server/index.js
@@ -4,19 +4,20 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const cors = require("cors");
+const router = express.Router();
 const mailRoute = require("./Routes/MailRoute");
 const projectRoute = require("./Routes/ProjectRoute");
 const blogsRoute = require("./Routes/BlogsRoute");
 const productRoute = require("./Routes/ProductRoute");
 
 app.use(express.json());
+app.use(router);
 
 // routes
 app.use(
 	cors({
 		origin: [
 			"https://quaint.kitchen360.co.ke",
-			"https://quaint.kitchen360.co.ke/",
 			"http://localhost:5173",
 		],
 	})
