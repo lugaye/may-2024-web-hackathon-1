7cca1e28b45c3b0c5bbe2efd497814fc7d4a73bd
Victor Kipkorir <victorquaint@gmail.com>
2023-07-17 12:50:21 +0300

    Crashed fixing

--- Server/index.js
+++ Server/index.js
@@ -3,38 +3,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const mongoose = require("mongoose");
-const cors = require("cors");
 
 app.use(express.json());
 
-// Enable CORS for specified origins
-app.use(
-	cors({
-		origin: ["https://quaint.kitchen360.co.ke", "http://localhost:5173"],
-	})
-);
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
-// Connect to MongoDB using promises
-mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log("MongoDB connected");
-		app.get("/", (req, res) => {
-			res.send("Hello World!");
-		});
-
-		// Start the server
-		app.listen(port, () => {
-			console.log(`Listening on port ${port}`);
-		});
-	})
-	.catch((err) => {
-		console.log("Error connecting to MongoDB:");
-		console.error(err);
-	});
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
 
 module.exports = app;
