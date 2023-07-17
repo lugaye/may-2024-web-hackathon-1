29fa3c8b253c71c18020be432ac369fd0b3e7c83
Victor Kipkorir <victorquaint@gmail.com>
2023-07-17 13:05:27 +0300

    Crashed fixing

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,23 @@
-// index.js
-const express = require("express");
-const app = express();
 require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const mailRoute = require("./Routes/MailRoute");
 
+// set up server
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
-
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-app.use("/api", mailRoute);
+app.use(
+	cors({
+		origin: ["https://quaint.kitchen360.co.ke", "http://localhost:5173"],
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+		credentials: true,
+	})
+);
+app.use(cookieParser());
 
 // connect to MongoDB using promises
 mongoose
@@ -21,16 +26,12 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then(() => {
-		console.log("MongoDB connected");
+		console.log("MongoDB connection established");
+		// set up routes after the database connection is established
+		app.use("/api", mailRoute);
+
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 	})
 	.catch((err) => {
-		console.log("Error connecting to MongoDB:");
-		console.error(err);
+		console.error("MongoDB connection error:", err);
 	});
-
-// start the server
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
-
-module.exports = app;
