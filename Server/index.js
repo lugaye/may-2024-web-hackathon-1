24655e04278e7c7a3c480f98ccf0267a322bc739
Victor Kipkorir <victorquaint@gmail.com>
2023-07-17 13:30:01 +0300

    Crashed fixing

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,29 @@
-require("dotenv").config();
 const express = require("express");
+const app = express();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const mailRoute = require("./Routes/MailRoute");
-
-// set up server
-const app = express();
-const PORT = process.env.PORT || 5000;
+const projectRoute = require("./Routes/ProjectRoute");
+const blogsRoute = require("./Routes/BlogsRoute");
+const productRoute = require("./Routes/ProductRoute");
 
 app.use(express.json());
+
+// routes
 app.use(
 	cors({
-		origin: ["https://quaint.kitchen360.co.ke", "http://localhost:5173"],
-		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-		credentials: true,
+		origin: [
+			"https://quaint.kitchen360.co.ke",
+			"https://quaint.kitchen360.co.ke/",
+			"http://localhost:5173",
+		],
 	})
 );
-app.use(cookieParser());
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
 // connect to MongoDB using promises
 mongoose
@@ -26,12 +32,19 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then(() => {
-		console.log("MongoDB connection established");
-		// set up routes after the database connection is established
+		console.log("MongoDB connected");
+		// routes
 		app.use("/api", mailRoute);
+		app.use("/api/projects", projectRoute);
+		app.use("/api/blogs", blogsRoute);
+		app.use("/api/products", productRoute);
 
-		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+		// start the server
+		const server = app.listen(process.env.PORT || 5000, () => {
+			console.log(`Listening on port ${server.address().port}`);
+		});
+
+		// Export the server instance
+		module.exports = server;
 	})
-	.catch((err) => {
-		console.error("MongoDB connection error:", err);
-	});
+	.catch((err) => console.log(err));
