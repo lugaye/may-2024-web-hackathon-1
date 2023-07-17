d9eb7f0f6d7f46bf77212d42dcdd8c73fd7bb356
Victor Kipkorir <victorquaint@gmail.com>
2023-07-17 16:35:47 +0300

    server crashed

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
-const cors = require("cors");
+const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
-// Middleware
+// set up server
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-// Connect to MongoDB
+// connect to MongoDB using promises
 mongoose
 	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
@@ -17,51 +18,14 @@ mongoose
 	})
 	.then(() => {
 		console.log("MongoDB connection established");
-		// routes
-		app.use("/api", require("./Routes/MailRoute.js"));
-		app.use("/api/projects", require("./Routes/ProjectRoute.js"));
-		app.use("/api/blogs", require("./Routes/BlogsRoute.js"));
-		app.use("/api/products", require("./Routes/ProductRoute.js"));
+		// set up routes after the database connection is established
+		app.use("/api", require("./Routes/MailRoute"));
+		app.use("/api/projects", require("./Routes/ProjectRoute"));
+		app.use("/api/blogs", require("./Routes/BlogsRoute"));
+		app.use("/api/products", require("./Routes/ProductRoute"));
 
-		// Start the server
-		app.listen(port, () => {
-			console.log(`Server running on port ${port}`);
-		});
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 	})
 	.catch((err) => {
 		console.error("MongoDB connection error:", err);
 	});
-
-// Home route
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send("500: Internal Server Error");
-});
-
-// Error handling for routes
-app.use("/api", (err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({ error: "Internal Server Error" });
-});
-
-app.use("/api/projects", (err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({ error: "Internal Server Error" });
-});
-
-app.use("/api/blogs", (err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({ error: "Internal Server Error" });
-});
-
-app.use("/api/products", (err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({ error: "Internal Server Error" });
-});
-
-module.exports = app;
