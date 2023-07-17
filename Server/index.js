fca68401b3e6522e32adbc2a70f9e6716a8a957e
Victor Kipkorir <victorquaint@gmail.com>
2023-07-17 12:54:02 +0300

    Crashed fixing

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
+const mailRoute = require("./Routes/MailRoute");
 
 app.use(express.json());
 
@@ -10,6 +12,23 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.use("/api", mailRoute);
+
+// connect to MongoDB using promises
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("MongoDB connected");
+	})
+	.catch((err) => {
+		console.log("Error connecting to MongoDB:");
+		console.error(err);
+	});
+
+// start the server
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
