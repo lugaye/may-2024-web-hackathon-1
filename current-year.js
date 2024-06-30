fd32c0883082576f2b3991bc65d2eb3cfe01962e
alunyapatrick2022 <patrickalunya106@gmail.com>
2024-07-01 02:03:08 +0300

    [Completed Hackathon One Pager Personal Portfolio]

---
+++ current-year.js
@@ -0,0 +1,7 @@
+{
+     // Get the current year
+     const currentYear = new Date().getFullYear();
+
+     // Update the text content of the span element with the current year
+     document.getElementById('currentYear').textContent = currentYear;
+}
