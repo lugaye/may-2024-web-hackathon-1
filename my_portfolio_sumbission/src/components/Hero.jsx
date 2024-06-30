644221f1535fae0d9a87b84c9f83d94742aa530f
Evarmedia <mishakmanuel@gmail.com>
2024-06-30 15:33:37 +0100

    feat: added projects, experince and education section(code and style), also added images for the projects section, SPA is complete and ready to be deployed

--- my_portfolio_sumbission/src/components/Hero.jsx
+++ my_portfolio_sumbission/src/components/Hero.jsx
@@ -2,14 +2,14 @@
 import image from "../assets/me.jpg";
 const Hero = () => {
   return (
-    <div className='flex justify-between items-center px-10 my-8 sm:my-32 sm:mx-10 gap-4'>
-      <div className='sm:text-3xl text-sm font-semibold sm:space-y-2'>
+    <div className='py-10 flex justify-between items-center px-10 sm:px-44 my-8 sm:my-48 sm:mx-10 gap-4'>
+      <div className='sm:text-5xl text-sm font-semibold sm:space-y-2'>
         <h2>Hi👋🏾, </h2>
         <h2>My Name is</h2>
         <h2 className="text-blue-500">Mishak Mosimabale</h2>
         <h1>{`I'm a Software Engineer`}</h1>
       </div>
-      <div className='sm:w-[300px] w-[200px]'>
+      <div className='sm:w-[400px] w-[200px] animate-pulse'>
         <img
           src={image}
           alt=''
