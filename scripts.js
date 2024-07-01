f65ca63bc7c538a57182606b6ef7e160546e238c
John Karson <casonj254@gmail.com>
2024-07-01 14:23:22 -0700

    portifolio

--- scripts.js
+++ scripts.js
@@ -4,12 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
 		            alert('Message sent successfully!');
 		            document.getElementById('contact-form').reset();
 		        });
-});
-document.addEventListener('DOMContentLoaded', function () {
+});document.addEventListener('DOMContentLoaded', function () {
 	    document.getElementById('contact-form').addEventListener('submit', function (event) {
 		            event.preventDefault();
 		            alert('Message sent successfully!');
 		            document.getElementById('contact-form').reset();
 		        });
 });
-
