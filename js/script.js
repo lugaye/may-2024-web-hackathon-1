f33309466323204dbf3e3deef8124f78bd5b0298
Jdamour12 <jeandamourkubwimana0@gmail.com>
2024-07-01 11:57:43 +0200

    Apdating HTML and CSS files

--- js/script.js
+++ js/script.js
@@ -24,6 +24,20 @@ header.classList.toggle('sticky', window.scrollY > 100);
 
 /*========== swiper ==========*/
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
 
 /*========== dark light mode ==========*/
 
