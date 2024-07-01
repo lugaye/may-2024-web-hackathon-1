5d38e4886974baa1e1ea0c6c6e6138eecd430c24
Ochieng-Onyango <ochiengarnold002@gmail.com>
2024-07-01 16:16:35 +0300

    attempt on may 2024 hackerthon

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+        }
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+        }
