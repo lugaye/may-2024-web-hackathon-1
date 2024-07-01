da4e485580a03aa2cacc9391ca1ebcf3760c99f9
Jdamour12 <jeandamourkubwimana0@gmail.com>
2024-07-01 09:34:39 +0200

    Apdating HTML and CSS files

--- js/script.js
+++ js/script.js
@@ -12,7 +12,11 @@ window.onscroll = () => {
 
     /*========== sticky navbar ==========*/
 
+window.onscroll = () => {
+let header = document.querySelector('.header');
 
+header.classList.toggle('sticky', window.scrollY > 100);
+};
     /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 
 };
