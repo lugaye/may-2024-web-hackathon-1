4e104a0b990ab2d5cbf630e68c97b2daef95edc2
Jdamour12 <jeandamourkubwimana0@gmail.com>
2024-07-01 07:42:54 +0200

    Creating the Javascript file of my Portfolio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*========== menu icon navbar ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+/*========== scroll sections active link ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+
+    /*========== sticky navbar ==========*/
+
+
+    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+
+};
+
+
+/*========== swiper ==========*/
+
+
+/*========== dark light mode ==========*/
+
+
+/*========== scroll reveal ==========*/
