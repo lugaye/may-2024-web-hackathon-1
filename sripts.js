9da4fb8a4d8a8e7cca7ea5edf6fe69b0b919b535
John Karson <casonj254@gmail.com>
2024-07-01 14:15:07 -0700

    portifolio project

---
+++ sripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+	    document.getElementById('contact-form').addEventListener('submit', function (event) {
+		            event.preventDefault();
+		            alert('Message sent successfully!');
+		            document.getElementById('contact-form').reset();
+		        });
+});
+document.addEventListener('DOMContentLoaded', function () {
+	    document.getElementById('contact-form').addEventListener('submit', function (event) {
+		            event.preventDefault();
+		            alert('Message sent successfully!');
+		            document.getElementById('contact-form').reset();
+		        });
+});
+
